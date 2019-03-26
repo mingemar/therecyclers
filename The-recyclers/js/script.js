e1ec72239ba52ffc527c74a9ac6d77721a3ee6b4
mingemar <mathildaingemarsson@gmail.com>
2019-03-26 09:16:02 +0000

    last edit

--- The-recyclers/js/script.js
+++ The-recyclers/js/script.js
@@ -1,1 +1,6 @@
-java
+$("#submitbutton").on("click", function() {
+ alert("Thank you for your enquiry. We will get back to you within 24h. ")
+ });
+$("#submitbuttonbike").on("click", function() {
+  alert("Thank you for your submitting your interest in becoming a Recycler. We will review your application and get back to you shortly. ")
+  });
